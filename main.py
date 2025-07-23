c55ede070b15cbfee359d2b9ff2495cecd487bd2
andersongomes <anderson@local>
2025-07-23 16:31:42 -0300

    fix: correção de um erro no info.json, usuários podem criar JSON inválidos.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import threading
 import time
 from flask_cors import CORS
+import re
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +16,49 @@ RAW_BASE_URL = "https://raw.githubusercontent.com/zanfranceschi/rinha-de-backend
 ranking_cache = []
 cache_lock = threading.Lock()
 
+mapa_linguagens = {
+    "node": "Node.js",
+    "node.js": "Node.js",
+    "nodejs": "Node.js",
+    "node js": "Node.js",
+    "javascript": "JavaScript",
+    "js": "JavaScript",
+    "typescript": "TypeScript",
+    "ts": "TypeScript",
+    "java": "Java",
+    "go": "Go",
+    "golang": "Go",
+    "rust": "Rust",
+    "elixir": "Elixir",
+    "c++": "C++",
+    "cpp": "C++",
+    "c#": "C#",
+    "c sharp": "C#",
+    "php": "PHP",
+    "python": "Python",
+    "swift": "Swift",
+    "lua": "Lua",
+    "ava": "Java",
+    "spring": "Spring",
+    "springboot": "Spring",
+    "nest": "NestJS",
+    "dotnet": ".NET",
+    ".net": ".NET",
+    "bun": "Bun",
+    "bun/typescript": "Bun",
+    "luvit": "Luvit",
+    "react": "React",
+    "angular": "Angular",
+    "vue": "Vue.js",
+    "hibernate": "Hibernate",
+    "django": "Django",
+    "flask": "Flask",
+    "express": "Express.js",
+    "nginx": "Nginx",
+    "redis": "Redis"
+}
+
+
 def buscar_participantes():
     response = requests.get(GITHUB_URL)
     participantes = []
@@ -24,18 +68,23 @@ def buscar_participantes():
         if datatarget:
             items = json.loads(datatarget[0].text)["payload"]["tree"]["items"]
             for item in items:
-                name = item["name"]
-                url_results = f"{RAW_BASE_URL}/{name}/partial-results.json"
-                url_info = f"{RAW_BASE_URL}/{name}/info.json"
-                response_results = requests.get(url_results)
-                response_info = requests.get(url_info)
-                if (response_results.status_code == 200 and len(response_results.content) > 0) and (response_info.status_code == 200 and len(response_info.content) > 0):
-                    data_results = json.loads(response_results.content)
-                    data_info = json.loads(response_info.content)
-                    data_info["data"] = data_results
-                    #print(data_info)
-                    participantes.append(data_info)
+                try:
+                    name = item["name"]
+                    url_results = f"{RAW_BASE_URL}/{name}/partial-results.json"
+                    url_info = f"{RAW_BASE_URL}/{name}/info.json"
+                    response_results = requests.get(url_results)
+                    response_info = requests.get(url_info)
+                    if (response_results.status_code == 200 and len(response_results.content) > 0) and (
+                            response_info.status_code == 200 and len(response_info.content) > 0):
+                        data_results = json.loads(re.sub(r',(\s*[}\]])', r'\1', response_results.content.decode()))
+                        data_info = json.loads(re.sub(r',(\s*[}\]])', r'\1', response_info.content.decode()))
+                        data_info["data"] = data_results
+                        data_info["langs"] = [(mapa_linguagens.get(lang) or lang) for lang in data_info["langs"]]
+                        participantes.append(data_info)
+                except Exception as e:
+                    print(f"Erro pegar dados : {e}")
     return participantes
+
 
 def atualizar_cache():
     global ranking_cache
@@ -56,6 +105,7 @@ def atualizar_cache():
 def home():
     return "API da Rinha está rodando! Use /ranking para ver os dados."
 
+
 @app.route('/ranking')
 def ranking():
     if not ranking_cache:
@@ -67,6 +117,7 @@ def agendador():
     while True:
         atualizar_cache()
         time.sleep(300)  # 5 minutos
+
 
 threading.Thread(target=agendador, daemon=True).start()
 
